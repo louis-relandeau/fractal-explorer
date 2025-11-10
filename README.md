ae7b7e78667bdada8e8f8c9ad90cffba9e9768dc
louis relandeau <nonmerci77@gmail.com>
2025-11-10 23:13:45 +0100

    mouse control, sonic level stuff

---
+++ README.md
@@ -0,0 +1,5 @@
+# TODOs
+
+* moving around should only recompute the new area
+* change the color palette
+* compute more detailed where required, otherwise interpolate
--- src/main.cpp
+++ src/main.cpp
@@ -53,10 +53,54 @@ int main() {
     sf::Texture texture(image);
     sf::Sprite sprite(texture);
 
+    bool needsUpdate = false;
+    bool dragging = false;
+    sf::Vector2i lastMousePos;
+
     while (window.isOpen()) {
-        for (auto event = window.pollEvent(); event.has_value(); event = window.pollEvent()) {
-            if (event->is<sf::Event::Closed>())
+        while (auto event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
                 window.close();
+            }
+            if (auto* e = event->getIf<sf::Event::MouseWheelScrolled>()) {
+                double oldZoom = zoom;
+                if (e->delta > 0) zoom *= 1.2;
+                else if (e->delta < 0) zoom /= 1.2;
+                sf::Vector2i mouse = sf::Mouse::getPosition(window);
+                double mx = (mouse.x - width / 2.0) * (4.0 / width) / oldZoom + offsetX;
+                double my = (mouse.y - height / 2.0) * (4.0 / height) / oldZoom + offsetY;
+                offsetX = mx - (mouse.x - width / 2.0) * (4.0 / width) / zoom;
+                offsetY = my - (mouse.y - height / 2.0) * (4.0 / height) / zoom;
+                needsUpdate = true;
+            }
+            if (auto* e = event->getIf<sf::Event::MouseButtonPressed>()) {
+                if (e->button == sf::Mouse::Button::Left) {
+                    dragging = true;
+                    lastMousePos = sf::Mouse::getPosition(window);
+                }
+            }
+            if (auto* e = event->getIf<sf::Event::MouseButtonReleased>()) {
+                if (e->button == sf::Mouse::Button::Left) {
+                    dragging = false;
+                }
+            }
+            if (event->is<sf::Event::MouseMoved>() && dragging) {
+                sf::Vector2i mouse = sf::Mouse::getPosition(window);
+                int dx = mouse.x - lastMousePos.x;
+                int dy = mouse.y - lastMousePos.y;
+                offsetX -= dx * (4.0 / width) / zoom;
+                offsetY -= dy * (4.0 / height) / zoom;
+                lastMousePos = mouse;
+                needsUpdate = true;
+            }
+        }
+
+        if (needsUpdate) {
+            renderFractal(image, mandelbrot, maxIter, zoom, offsetX, offsetY);
+            if (texture.loadFromImage(image)) {
+                sprite.setTexture(texture);
+            }
+            needsUpdate = false;
         }
 
         window.clear();
